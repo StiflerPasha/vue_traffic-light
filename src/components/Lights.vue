3f53b3523ba38babab7cd8cae1bb7dc802d13f8b
Pasha_HOME <dvoynospavel@gmail.com>
2019-09-11 22:50:57 +0300

    timer // some CSS

--- src/components/Lights.vue
+++ src/components/Lights.vue
@@ -1,85 +1,103 @@
 <template>
-		<div>
-				<pre @click="countDown">{{counter}}</pre>
-				<div class="red" v-bind:class="{active: this.fire === 'red'}"></div>
-				<div class="yellow" v-bind:class="{active: this.fire === 'yellow'}"></div>
-				<div class="green" v-bind:class="{active: this.fire === 'green'}"></div>
-		</div>
+  <div class="lights">
+    <div class="light red"
+         v-bind:class="{active: this.fire === 'red'}"></div>
+    <div class="light yellow"
+         v-bind:class="{active: this.fire === 'yellow'}">
+      <pre class='timer'
+           @click="countDown"
+           v-bind:style="{color: this.fire}"
+           v-if="this.fire !== 'yellow'">{{counter}}</pre>
+    </div>
+    <div class="light green"
+         v-bind:class="{active: this.fire === 'green'}"></div>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'lights',
-    props: ['fire', 'count'],
-    data() {
-      return {
-        counter: 0,
-      };
-    },
-    methods: {
-      countDown() {
-        if (this.counter) {
-          return setTimeout(() => {
-            --this.counter;
-            this.countDown();
-          }, 1000);
-        } else {
-          switch (this.$route.name) {
-            case 'red': {
-              this.$router.push({ name: 'yellow' });
-              break;
-            }
-            case 'yellow': {
-              this.$router.push({ name: 'green' });
-              break;
-            }
-            case 'green': {
-              this.$router.push({ name: 'red' });
-              break;
-            }
-            default:
-              return false;
-          }
-        }
-      },
-    },
-    mounted() {
-      this.counter = this.count;
-      this.countDown();
-    },
-  };
+   export default {
+	  name: 'lights',
+	  props: ['fire', 'count'],
+	  data() {
+		 return {
+			counter: 0,
+		 };
+	  },
+	  methods: {
+		 countDown() {
+			if (this.counter) {
+			   return setTimeout(() => {
+				  --this.counter;
+				  this.countDown();
+			   }, 1000);
+			} else {
+			   //this.$router.afterEach((to, from, next) => console.log(from));
+			   switch (this.$route.name) {
+				  case 'red': {
+					 this.$router.push({ name: 'yellow' });
+					 break;
+				  }
+				  case 'yellow': {
+					 this.$router.push({ name: 'green' });
+					 break;
+				  }
+				  case 'green': {
+					 this.$router.push({ name: 'red' });
+					 break;
+				  }
+				  default:
+					 return false;
+			   }
+			}
+		 },
+	  },
+	  mounted() {
+		 this.counter = this.count;
+		 this.countDown();
+	  },
+   };
 </script>
 
 
 <style scoped>
-		.red {
-				width: 100px;
-				height: 100px;
-				border-radius: 50%;
-				background: red;
-				margin: 15px;
-				opacity: 0.25;
-		}
+  .lights {
+    width: 140px;
+    margin: 15vh auto;
+    padding: 10px;
+    border: 2px solid wheat;
+    border-radius: 25px;
+    background: #141212;
+    box-shadow: inset 2px 2px 5px rgba(154, 147, 140, 0.5), 1px 1px 5px rgba(255, 255, 255, 1);
+  }
 
-		.yellow {
-				width: 100px;
-				height: 100px;
-				border-radius: 50%;
-				background: orange;
-				margin: 15px;
-				opacity: 0.25;
-		}
+  .light {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    margin: 10px;
+    opacity: 0.25;
+  }
 
-		.green {
-				width: 100px;
-				height: 100px;
-				border-radius: 50%;
-				background: green;
-				margin: 15px;
-				opacity: 0.25;
-		}
+  .red {
+    background: red;
+  }
 
-		.active {
-				opacity: 1;
-		}
+  .yellow {
+    background: orange;
+  }
+
+  .green {
+    background: green;
+  }
+
+  .active {
+    opacity: 1;
+  }
+
+  .timer {
+    text-align: center;
+    line-height: 100px;
+    font-size: 4em;
+    font-weight: bolder;
+  }
 </style>
