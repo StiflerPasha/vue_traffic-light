9cb25e6da1aef478b5b0f0354c3d592be298f0ec
Pasha_HOME <dvoynospavel@gmail.com>
2019-09-11 22:48:39 +0300

    check path before mount // some CSS

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,24 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/1">Red</router-link> |
-      <router-link to="/2">Yellow</router-link> |
-      <router-link to="/3">Green</router-link>
-    </div>
-    <router-view/>
-  </div>
+    <router-view />
 </template>
 
 <script>
-  export default {
-    mounted() {
-      this.$router.push({ path: '/1' });
-    }
-  }
+   export default {
+	  mounted() {
+		 this.$route.path === '/' && this.$router.push({ path: '/1' });
+	  },
+   };
 
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  body {
+    background: #242424
+  }
 </style>
