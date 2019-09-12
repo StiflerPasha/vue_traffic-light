50a410513df1cdfa0eec1f2a88833fb742ddbf4b
Pasha_HOME <dvoynospavel@gmail.com>
2019-09-12 20:35:40 +0300

    refactor

--- src/views/Green.vue
+++ src/views/Green.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <Lights fire="green" count='15'/>
+    <Lights fire="green" count='15' />
   </div>
 </template>
 
 <script>
-import Lights from '../components/Lights';
+   import Lights from '../components/Lights';
 
-export default {
-  name: 'green',
-  components: {
-    Lights,
-  },
-};
+   export default {
+	  name: 'green',
+	  components: {
+		 Lights,
+	  },
+   };
 </script>
--- src/views/Red.vue
+++ src/views/Red.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <Lights fire="red" count='10'/>
+    <Lights fire="red" count='10' />
   </div>
 </template>
 
 <script>
-import Lights from '../components/Lights';
+   import Lights from '../components/Lights';
 
-export default {
-  name: 'red',
-  components: {
-    Lights,
-  },
-};
+   export default {
+	  name: 'red',
+	  components: {
+		 Lights,
+	  },
+   };
 </script>
